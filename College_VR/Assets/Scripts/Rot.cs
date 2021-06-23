20676e7e79bc5ba1ad7b2e019f29cba2e8653ced
EjHam98 <66335818+EjHam98@users.noreply.github.com>
2021-06-23 05:18:02 +0300

    Update Rot.cs

--- College_VR/Assets/Scripts/Rot.cs
+++ College_VR/Assets/Scripts/Rot.cs
@@ -10,17 +10,24 @@ public class Rot : MonoBehaviour
     private const float surface = 0.48f;
     private const float rope_length = 12.5f; //0.8m
 
-    private Vector3 eulerAngs1, eulerAngs2;
+    private Vector3 eulerAngs, eulerAngs1, eulerAngs2;
     private float total_mass;
 
     public int liters_of_paint = 18;
-    public float velocity = 0f;
-    public float r = 0.1f;
+    private float velocity = 0f;
+    private float r = 0.1f;
 
-    public float v1 = 0f;
-    public float v2 = 0f;
-    public float theta_max1 = 60f;
-    public float theta_max2 = 60f;
+    private float v1 = 0f;
+    private float v2 = 0f;
+    private float theta_max1 = 60f;
+    private float theta_max2 = 60f;
+
+    private float theta = 45f;
+    private float phi = -90f;
+    private float vtheta = 0f;
+    private float vphi = 0f;
+    private float atheta = 0f;
+    private float aphi = 0f;
 
     public int randr = 3;
     public float prand = 0.1f;
@@ -30,115 +37,74 @@ public class Rot : MonoBehaviour
     private PhysicsCode PhysicsCalc = null;
 
     // Start is called before the first frame update
-    void Start1()
-    {
-        total_mass = bucket_mass + liters_of_paint * paint_liter_mass;
-        eulerAngs1 = Vector3.zero;
-        eulerAngs2 = Vector3.zero;
-        transform.localScale = new Vector3(r, r, r);
-        Vector3 curpos = transform.localPosition;
-        curpos = curpos + new Vector3(prand * Random.Range(-1 * randr, randr), prand * Random.Range(-1 * randr, randr), prand * Random.Range(-1 * randr, randr));
-        transform.localPosition = curpos;
-        PhysicsCalc = new PhysicsCode();
-    }
-
-    private float abs_dbl(float x)
-    {
-        if (x < 0)
-        {
-            return -1.0f * x;
-        }
-        return x;
-    }
-
-    // Update is called once per frame
-    //void Update1()
-    //{
-    //    float t = Time.deltaTime;
-    //    if (transform.localPosition.y < -1 || transform.localPosition.y > 100)
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //    float total_force = PhysicsCalc.getGravity(mass) - PhysicsCalc.getAirResistance(surface, velocity);
-    //    float acceleration = total_force / mass;
-    //    float speed = velocity + abs_dbl(acceleration) * t;
-    //    if (acceleration >= 0)
-    //    {
-    //        transform.localPosition = new Vector3((float)transform.localPosition.x, (float)(transform.localPosition.y - speed * t), (float)transform.localPosition.z);
-    //    }
-    //    else
-    //    {
-    //        transform.localPosition = new Vector3((float)transform.localPosition.x, (float)(transform.localPosition.y + speed * t), (float)transform.localPosition.z);
-    //    }
-    //    velocity = speed;
-    //}
-
     void Start()
     {
-        //transform.Rotate(0f, 0f, 45f, Space.World);
-        //transform.Rotate(0f, 45f, 0f, Space.World);
-        //transform.Rotate(45f, 0f, 0f, Space.World);
-        //transform.rotation = transform.rotation * Quaternion.Euler(0f, 0f, 45f);
-        //transform.rotation = transform.rotation * Quaternion.Euler(0f, 45f, 0f);
-        //transform.rotation = transform.rotation * Quaternion.Euler(45f, 0f, 0f);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, 0f);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, 0f);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, 0f);
-
         
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(-45f, 0f, 0f);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, -45f);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 90f, 0f);
-
-        //Debug.Log(transform.eulerAngles);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(30f, 0f, 0f);
-        //Debug.Log(transform.eulerAngles);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 30f, 0f);
-        //Debug.Log(transform.eulerAngles);
-        //transform.eulerAngles = transform.eulerAngles + new Vector3(0f, 0f, -45f);
 
         total_mass = bucket_mass + liters_of_paint * paint_liter_mass;
-        eulerAngs1 = Vector3.zero;
-        eulerAngs2 = Vector3.zero;
-        curtime = 0f;
 
-        eulerAngs1 = new Vector3(theta_max1, 0f, 0f);
-        eulerAngs2 = new Vector3(0f, theta_max2, 0f);
+        eulerAngs = new Vector3(0f, -1f*phi, -1f*theta);
 
-        //transform.GetChild(0).eulerAngles = eulerAngs2;
-        transform.eulerAngles = eulerAngs1;
+        transform.Rotate(Vector3.forward, eulerAngs.z, Space.World);
+        transform.Rotate(Vector3.up, eulerAngs.y, Space.World);
 
     }
 
     void Update()
     {
-        ////-g sin(theta)
+        // 90y 10theta
+
+        if(phi < 90)
+        {
+            phi += 0.9f;
+            transform.Rotate(Vector3.up, 0.9f, Space.World);
+            transform.Rotate(Vector3.forward, 0.3f, Space.Self);
+        }
+
+        // - 30 * abs(cos(phi))
+        //phi += 4f;
+
+        //float tmp = theta;
+        //theta = 20f + 45f * Mathf.Cos(phi / 180f);
+
+        //eulerAngs = new Vector3(0f, -1f * phi, -1f * theta);
+
+        //transform.Rotate(Vector3.right, 4f, Space.Self);
+        //transform.Rotate(Vector3.right, tmp - theta, Space.Self);
+        //float tmp = theta;
+        //atheta = vphi * vphi * Mathf.Sin(theta/180f) * Mathf.Cos(theta/180f) - 9.8f / 0.8f * Mathf.Sin(theta/180f);
+        //aphi = -2 * vtheta * vphi * (Mathf.Cos(theta/180f) / Mathf.Sin(theta/180f));
+        //vtheta = vtheta + atheta *0.2f;
+        //vphi = vphi + aphi * 0.2f;
+        //theta = theta + vtheta * 0.2f;
+        //phi = phi + vphi * 0.2f;
+        //phi = phi - tmp + theta;
+        //eulerAngs = new Vector3(eulerAngs.x, -1f*phi, -1f*theta);
         //transform.eulerAngles = eulerAngs;
-        //curtime += Time.deltaTime;
-        ////float force = total_mass * 9.8f * Mathf.Sin(eulerAngs.x / 180f);
-        //float acc = -0.0007f * total_mass * 9.8f * Mathf.Sin(eulerAngs.x/180f);
-        ////velocity = velocity + acc * 0.02f;
-        //velocity += acc;
-        
-        //eulerAngs = new Vector3(eulerAngs.x + velocity, 0f, 0f);
-
-        //-g sin(theta)
-
-        //float acc2 = -1f * total_mass * 9.8f * Mathf.Sin(eulerAngs2.y / 180f) / 0.8f;
-        ////velocity = velocity + acc2 * 0.02f;
-        //v2 = v2 + acc * 0.02f;
-
-        //eulerAngs2 = new Vector3(eulerAngs2.x, eulerAngs2.y + v2 * 0.02f, eulerAngs2.z);
-
-        //transform.GetChild(0).eulerAngles = eulerAngs2;
-        
-        //float force = total_mass * 9.8f * Mathf.Sin(eulerAngs.x / 180f);
-        float acc = -1f * total_mass * 9.8f * Mathf.Sin(eulerAngs1.x / 180f) / 0.8f;
-        //velocity = velocity + acc * 0.02f;
-        v1 = v1 + acc * 0.02f;
-
-        eulerAngs1 = new Vector3(eulerAngs1.x + v1 * 0.02f, eulerAngs1.y, eulerAngs1.z);
-
-        transform.eulerAngles = eulerAngs1;
     }
 }
+    //void Start()
+    //{
+    //    total_mass = bucket_mass + liters_of_paint * paint_liter_mass;
+    //    eulerAngs1 = Vector3.zero;
+    //    eulerAngs2 = Vector3.zero;
+    //    curtime = 0f;
+
+    //    eulerAngs1 = new Vector3(theta_max1, 0f, 0f);
+    //    eulerAngs2 = new Vector3(0f, theta_max2, 0f);
+
+    //    //transform.GetChild(0).eulerAngles = eulerAngs2;
+    //    transform.eulerAngles = eulerAngs1;
+
+    //}
+
+    //void Update()
+    //{
+
+    //    float acc = -1f * total_mass * 9.8f * Mathf.Sin(eulerAngs1.x / 180f) / 0.8f;
+    //    v1 = v1 + acc * 0.02f;
+
+    //    eulerAngs1 = new Vector3(eulerAngs1.x + v1 * 0.02f, eulerAngs1.y, eulerAngs1.z);
+
+    //    transform.eulerAngles = eulerAngs1;
+    //}
