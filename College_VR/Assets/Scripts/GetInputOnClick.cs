5af409497dc003a4cfc776e11665348241c3fafb
EjHam98 <66335818+EjHam98@users.noreply.github.com>
2021-06-28 01:34:33 +0300

    Bayan's Changes

---
+++ College_VR/Assets/Scripts/GetInputOnClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetInputOnClick : MonoBehaviour
+{
+    
+    //get input filed
+    public InputField inputUser1;
+    public InputField inputUser2;
+    public InputField inputUser3;
+    public InputField inputUser4;
+
+    public Button btnClick;
+
+    private void Start()
+    {
+        //attach button event 
+        btnClick.onClick.AddListener(GetInputOnClickHandler);
+    }
+
+//<summary>
+
+    // Method that will run on click
+
+// </summary>
+    public void GetInputOnClickHandler()
+    {
+        //show input text when we click the button
+        Debug.Log("Log input: " + inputUser1.text);
+        Debug.Log("Log input: " + inputUser2.text);
+        Debug.Log("Log input: " + inputUser3.text);
+        Debug.Log("Log input: " + inputUser4.text);
+    }
+}
